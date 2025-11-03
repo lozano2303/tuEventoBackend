b11c3828817df05bff743ca4960ad4b7f73bdbaf
Cristofer Lozano <cristoferlozano233@gmail.com>
2025-11-02 20:25:32 -0500

    feat(backend): add initial backend folder structure

---
+++ backend/src/main/java/com/capysoft/backend/TuEventoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.capysoft.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuEventoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuEventoBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/capysoft/backend/TuEventoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capysoft.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuEventoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
